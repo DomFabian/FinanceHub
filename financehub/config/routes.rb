9ba72ecd33904cdae6249b3460f1ecb01fb50ffd
Dominick Fabian <dominickfabian@gmail.com>
2019-04-01 21:01:44 +0000

    add SEC template

--- financehub/config/routes.rb
+++ financehub/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  
+  get 'static_pages/about', to: 'static_pages#about'
+  get 'static_pages/help', to: 'static_pages#help'
 end
