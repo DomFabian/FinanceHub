430c94e7575487648f7cf15cd96a5d3b88ef7e3d
Dominick Fabian <dominickfabian@gmail.com>
2019-04-04 02:39:14 +0000

    fix db index renaming

--- db/migrate/20190404014050_fix_columns_in_join_table.rb
+++ db/migrate/20190404014050_fix_columns_in_join_table.rb
@@ -1,7 +1,7 @@
 class FixColumnsInJoinTable < ActiveRecord::Migration[5.2]
   def change
     rename_column :form_responses_subcategories, :form_responses_id, :form_response_id
-    rename_index :form_responses_subcategories, 'index_form_responses_subcategories_on_form_responses_id',
-                                                'index_form_responses_subcategories_on_form_response_id'
+    #rename_index :form_responses_subcategories, 'index_form_responses_subcategories_on_form_responses_id',
+    #                                            'index_form_responses_subcategories_on_form_response_id'
   end
 end
